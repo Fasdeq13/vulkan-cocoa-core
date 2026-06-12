4f654486d9e8b718c097a60c24996ff15b12d056
Fasdeq13 <wentisoff@gmail.com>
2026-06-12 14:38:48 +0300

    Add files via upload

--- Backend/include/QuartzMetalBackend.h
+++ Backend/include/QuartzMetalBackend.h
@@ -6,6 +6,32 @@
 #include <string>
 #include <memory>
 
+#ifndef CGGEOMETRY_H_
+struct CGPoint {
+    double x;
+    double y;
+};
+
+struct CGSize {
+    double width;
+    double height;
+};
+
+struct CGRect {
+    struct CGPoint origin;
+    struct CGSize size;
+};
+#endif
+
+typedef double CGFloat;
+typedef uint32_t CGGlyph;
+typedef struct CGColor* CGColorRef;
+typedef struct CGContext* CGContextRef;
+typedef struct CGFont* CGFontRef;
+typedef struct CGImage* CGImageRef;
+typedef struct CGColorSpace* CGColorSpaceRef;
+typedef const struct __CFString* CFStringRef;
+
 struct BackendDeviceCaps {
     std::string deviceName;
     uint32_t vendorID;
@@ -51,7 +77,26 @@ public:
 #ifdef __cplusplus
 extern "C" {
 #endif
+
     VulkanRenderContext* GetGlobalVulkanContext();
+
+    CGColorSpaceRef CGColorSpaceCreateDeviceRGB(void);
+    CGColorSpaceRef CGColorSpaceCreateDeviceGray(void);
+    CGColorSpaceRef CGColorSpaceCreateWithName(CFStringRef name);
+    void CGColorSpaceRelease(CGColorSpaceRef space);
+
+    CGContextRef CGBitmapContextCreate(void* data, size_t width, size_t height, size_t bitsPerComponent, size_t bytesPerRow, CGColorSpaceRef space, uint32_t bitmapInfo);
+    void CGContextRelease(CGContextRef c);
+    void CGContextClearRect(CGContextRef c, CGRect rect);
+    void CGContextSetRGBFillColor(CGContextRef c, CGFloat red, CGFloat green, CGFloat blue, CGFloat alpha);
+    void CGContextFillRect(CGContextRef c, CGRect rect);
+
+    CGFontRef CGFontCreateWithDataProvider(void* provider);
+    CGFontRef CGFontCreateWithFontName(CFStringRef name);
+    void CGFontRelease(CGFontRef font);
+
+    int NSApplicationMain(int argc, const char* argv[]);
+
 #ifdef __cplusplus
 }
 #endif
